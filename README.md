c6ef9367531a444b3ee91d32ae7731f0d2b620af
sandroid <sandroid@posteo.net>
2024-10-27 20:51:06 +0100

    Control music playback using the knob

--- README.md
+++ README.md
@@ -10,4 +10,7 @@ Custom keymap for the Keychron Q3 Max.
 * Adds number keys to the home row of the `WIN_FN` layer to allow fast number typing without a numpad.
 * Uses [layer tab toggle](https://docs.qmk.fm/feature_layers#switching-and-toggling-layers) for the `Fn` key and activate it with 2 taps for faster switching to the `WIN_FN` layer.
 * Enables [Caps Word](https://docs.qmk.fm/features/caps_word)
-* Remaps the knob button to play/pause playback
+* Uses [tap dance](https://docs.qmk.fm/features/tap_dance#tap-dance-a-single-key-can-do-3-5-or-100-different-things) to control music with the rotary knob:
+    * **Single tap:** Play/Pause track
+    * **Double tap:** Next track
+    * **Triple tap**: Previous track
--- keymap.c
+++ keymap.c
@@ -8,6 +8,25 @@ enum layers {
     WIN_FN,
 };
 
+enum tap_dance {
+    TD_ENC,
+};
+
+void dance_playback(tap_dance_state_t *state, void *user_data) {
+    switch (state->count) {
+        case 1:
+            tap_code(KC_MPLY); break;
+        case 2:
+            tap_code(KC_MNXT); break;
+        case 3:
+            tap_code(KC_MPRV); break;
+    }
+}
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_ENC] = ACTION_TAP_DANCE_FN(dance_playback),
+};
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_iso_88(
@@ -27,9 +46,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,   _______,  _______,   _______, _______),
 
     [WIN_BASE] = LAYOUT_iso_88(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,    KC_MPLY,  KC_PSCR,  KC_CTANA, RGB_MOD,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,    KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,   KC_ENT,   KC_DEL,   KC_END,   KC_PGDN,
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,    TD(TD_ENC), KC_PSCR,  KC_CTANA, RGB_MOD,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,    KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,   KC_ENT,     KC_DEL,   KC_END,   KC_PGDN,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,
         KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,             KC_RSFT,            KC_UP,
         KC_LCTL,  KC_LWIN,  KC_LALT,                               KC_SPC,                                  KC_RALT,  KC_RWIN,  TT(WIN_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
